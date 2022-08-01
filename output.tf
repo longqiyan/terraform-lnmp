fa2966320ec92ffb3f671e6e3a047bd242c6927d
longqiyan <77777005+longqiyan@users.noreply.github.com>
2022-08-01 14:51:12 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,22 @@
-output "public_ip" {
-  value = aws_instance.web.*.public_ip
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+# profile = "default"
+# AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  access_key                  = var.access_key
+  secret_key                  = var.secret_key
+  region  = var.region
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
+  instance_type = var.instance_type
 }
