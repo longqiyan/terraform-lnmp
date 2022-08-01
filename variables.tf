4cf62636a36d228398140ba5900e2ec64b26c576
longqiyan <77777005+longqiyan@users.noreply.github.com>
2022-08-01 14:55:51 +0800

    Update and rename output.tf to variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t1.micro"
+  description = "计算实例类型"
+}
+variable "access_key" {
+  default = "AKIAT3E3VI2SQXRWMNXH"
+  description = "AK"
+}
+variable "secret_key" {
+  default = "pxOWtnqeiasK2gOz87bKsNz/AoV8jvG301e1t1kf"
+  description = "SK"
+}
+variable "region" {
+  default = "us-east-1"
+}
