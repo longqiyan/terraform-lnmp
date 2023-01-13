efdfc70609da35af9631505ab08bcc8f992572b1
zhangjielong <zhangjielong@idcos.com>
2023-01-13 17:42:52 +0800

    腾讯云备份验证

--- tencentcloud/main.tf
+++ tencentcloud/main.tf
@@ -10,60 +10,60 @@ module "networking" {
   account_id = var.account_id
 }
 
-//module "instance"  {
-//  //source为对应module的虚拟机创建的代码存放地址
-//  source = "git::https://gitlab.yun.shop/iac-module/terraform-tencentcloud-modules.git//modules/instance?ref=v0.1.0"
-//  count  = var.instance_number
-//
-//  zone_id         = var.zone_id
-//  vpc_id          = module.networking.vpc.vpc_id
-//  subnet_id       = module.networking.subnets.public.subnet_id
-//  security_groups = module.networking.security_groups.public.security_group_id
-//  keypair_ids     = module.account.kye_pair.key_id
-//  instance_type = var.instance_type
-//  instance_name = var.instance_name
-//  image_id      = var.image_id
-//  hostname      = var.hostname
-//
-//  disk_type  = var.disk_type
-//  disk_size  = var.disk_size
-//  data_disks = var.data_disks
-//
-//  internet_bandwidth   = var.internet_bandwidth
-//  internet_charge_type = var.internet_charge_type
-//  private_ip           = var.private_ip
-//
-//  project_id = var.project_id
-//  tags       = var.tags
-//}
+module "instance"  {
+  //source为对应module的虚拟机创建的代码存放地址
+  source = "git::https://gitlab.yun.shop/iac-module/terraform-tencentcloud-modules.git//modules/instance?ref=v0.1.0"
+  count  = var.instance_number
 
-resource "tencentcloud_instance" "foo" {
-
-  availability_zone         = var.zone_id
+  zone_id         = var.zone_id
   vpc_id          = module.networking.vpc.vpc_id
   subnet_id       = module.networking.subnets.public.subnet_id
-  security_groups = [module.networking.security_groups.public.security_group_id]
+  security_groups = module.networking.security_groups.public.security_group_id
   keypair_ids     = module.account.kye_pair.key_id
   instance_type = var.instance_type
   instance_name = var.instance_name
-  key_ids       = [module.account.kye_pair.key_id]
-  private_ip           = var.private_ip
-  internet_max_bandwidth_out   = var.internet_bandwidth
-  internet_charge_type = var.internet_charge_type
   image_id      = var.image_id
   hostname      = var.hostname
-  system_disk_type   = var.disk_type
-  system_disk_size          = var.disk_size
-  allocate_public_ip         = var.internet_bandwidth > 0 ? true : false
 
-  data_disks {
-    data_disk_type = "CLOUD_SSD"
-    data_disk_size = 60
-    data_disk_snapshot_id = var.snapshot_id == "1" ? "" : var.snapshot_id
-  }
+  disk_type  = var.disk_type
+  disk_size  = var.disk_size
+  data_disks = var.data_disks
+
+  internet_bandwidth   = var.internet_bandwidth
+  internet_charge_type = var.internet_charge_type
+  private_ip           = var.private_ip
+
   project_id = var.project_id
   tags       = var.tags
 }
+
+//resource "tencentcloud_instance" "foo" {
+//
+//  availability_zone         = var.zone_id
+//  vpc_id          = module.networking.vpc.vpc_id
+//  subnet_id       = module.networking.subnets.public.subnet_id
+//  security_groups = [module.networking.security_groups.public.security_group_id]
+//  keypair_ids     = module.account.kye_pair.key_id
+//  instance_type = var.instance_type
+//  instance_name = var.instance_name
+//  key_ids       = [module.account.kye_pair.key_id]
+//  private_ip           = var.private_ip
+//  internet_max_bandwidth_out   = var.internet_bandwidth
+//  internet_charge_type = var.internet_charge_type
+//  image_id      = var.image_id
+//  hostname      = var.hostname
+//  system_disk_type   = var.disk_type
+//  system_disk_size          = var.disk_size
+//  allocate_public_ip         = var.internet_bandwidth > 0 ? true : false
+//
+//  data_disks {
+//    data_disk_type = "CLOUD_SSD"
+//    data_disk_size = 60
+//    data_disk_snapshot_id = var.snapshot_id == "1" ? "" : var.snapshot_id
+//  }
+//  project_id = var.project_id
+//  tags       = var.tags
+//}
 
 locals {
   hash = substr(parseint(sha1(var.cloudiac_env_id), 16), 0, 6)
@@ -127,17 +127,17 @@ resource "random_integer" "this" {
   }
 }
 
-//resource "tencentcloud_cbs_storage" "storage" {
-//  availability_zone = var.zone_id
-//  storage_size    = 60
-//  force_delete   =true
-//  storage_name    =  "test"
-//  storage_type = "CLOUD_SSD"
-//  // 如果查询不到 snapshot，这里的 id 值是 null
-//  snapshot_id       = var.snapshot_id == "1" ? "" : var.snapshot_id
-//}
-//
-//resource "tencentcloud_cbs_storage_attachment" "attachment" {
-//  storage_id  = tencentcloud_cbs_storage.storage.id
-//  instance_id = module.instance[0].instance.instance_id
-//}
+resource "tencentcloud_cbs_storage" "storage" {
+  availability_zone = var.zone_id
+  storage_size    = 60
+  force_delete   =true
+  storage_name    =  "test"
+  storage_type = "CLOUD_SSD"
+  // 如果查询不到 snapshot，这里的 id 值是 null
+  snapshot_id       = var.snapshot_id == "1" ? "" : var.snapshot_id
+}
+
+resource "tencentcloud_cbs_storage_attachment" "attachment" {
+  storage_id  = tencentcloud_cbs_storage.storage.id
+  instance_id = module.instance[0].instance.instance_id
+}
--- tencentcloud/outputs.tf
+++ tencentcloud/outputs.tf
@@ -1,53 +1,53 @@
-//output "home_url" {
-//  value = [
-//    for index, instance in module.instance :
-//    {
-//      "home_url" : instance.instance.private_ip,
-//    }
-//  ]
-//}
-//
-//output "username" {
-//  value = "admin"
-//}
-//
-//output "password" {
-//  value = "zaq1@WSX"
-//}
-//
-//
-//output "public_ip" {
-//  value = module.instance.*.instance.public_ip
-//}
-//
-//output "private_ip" {
-//  value = module.instance.*.instance.private_ip
-//}
-//
-//output "mysql_db" {
-//  value = var.mysql_db
-//}
-//
-//output "msyql_host" {
-//  value = var.mysql_host
-//}
-//
-//output "mysql_user" {
-//  value = var.mysql_user
-//}
-//
-//output "mysql_password" {
-//  value = var.mysql_password
-//}
-//
-//output "env_id" {
-//  value = var.cloudiac_env_id
-//}
-//
-//output "random_integer" {
-//  value = local.hash
-//}
-//
-//output "storage" {
-//  value = tencentcloud_cbs_storage.storage.id
-//}
+output "home_url" {
+  value = [
+    for index, instance in module.instance :
+    {
+      "home_url" : instance.instance.private_ip,
+    }
+  ]
+}
+
+output "username" {
+  value = "admin"
+}
+
+output "password" {
+  value = "zaq1@WSX"
+}
+
+
+output "public_ip" {
+  value = module.instance.*.instance.public_ip
+}
+
+output "private_ip" {
+  value = module.instance.*.instance.private_ip
+}
+
+output "mysql_db" {
+  value = var.mysql_db
+}
+
+output "msyql_host" {
+  value = var.mysql_host
+}
+
+output "mysql_user" {
+  value = var.mysql_user
+}
+
+output "mysql_password" {
+  value = var.mysql_password
+}
+
+output "env_id" {
+  value = var.cloudiac_env_id
+}
+
+output "random_integer" {
+  value = local.hash
+}
+
+output "storage" {
+  value = tencentcloud_cbs_storage.storage.id
+}
