5e46168c2b6a99fd7938943d775df8d6c7d47233
longqiyan <1424087925@qq.com>
2023-02-06 18:06:54 +0800

    ln命令测试

--- alicloud/ansible/cloudlego-docker/sql/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mysql -hlocalhost -uroot --default-character-set=utf8 -p${MYSQL_ROOT_PASSWORD} -e "
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
-"
-for i in $(find /docker-entrypoint-initdb.d -type f -name "*.sql.gz" | sort);do echo -e "\n"$i && gunzip < $i | mysql -hlocalhost -u${MYSQL_USER} --default-character-set=utf8 -p${MYSQL_PASSWORD};done
